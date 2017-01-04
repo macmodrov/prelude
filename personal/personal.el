989491e54a5064809a3e185959d0d766d92afe77
moderaum <M.Moderau@tt.com.pl>
2017-01-04 16:40:10 +0100

    Added some personal settings

---
+++ personal/personal.el
@@ -0,0 +1,10 @@
+(desktop-save-mode)
+(set-default 'truncate-lines t)
+(set-default 'standard-indent 2)
+
+(setq dired-listing-switches "-aDlG")
+(setq grep-command "grep -nHIiR -e")
+
+(put 'upcase-region 'disabled nil)
+(put 'scroll-left 'disabled nil)
+(put 'downcase-region 'disabled nil)
